bb12e3d6c1cba915a67b52db805bce9b5cb86f25
Chlorophyllkid <chlorophyllkid@gmail.com>
2019-06-19 16:27:09 +0200

    add postcss + 11ty.js stylesheet script

---
+++ src/assets/stylesheet.11ty.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+const precss = require('precss');
+const postcss = require('postcss');
+const postcssImport = require('postcss-import');
+const postcssMixins = require('postcss-mixins');
+const postcssColorMix = require('postcss-color-mix');
+const cssnano = require('cssnano');
+
+// the file name as an entry point for postcss compilation
+// also used to define the output filename in our output /css folder.
+const fileName = 'stylesheet.css';
+
+module.exports = class stylesheet {
+  async data() {
+    const rawFilepath = path.join(__dirname, `../_includes/${fileName}`);
+
+    return {
+      permalink: `css/${fileName}`,
+      rawFilepath,
+      rawCss: await fs.readFileSync(rawFilepath),
+    };
+  }
+
+  render({ rawCss, rawFilepath }) {
+    return postcss([precss, postcssImport, postcssMixins, postcssColorMix, cssnano])
+      .process(rawCss, { from: rawFilepath })
+      .then(result => result.css);
+  }
+};
