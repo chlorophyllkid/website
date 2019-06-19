4a90d0b3edd63fa9ee93b050a4971899b2fd0f15
Chlorophyllkid <chlorophyllkid@gmail.com>
2019-06-19 16:25:42 +0200

    add package basics

---
+++ config/minify-html.js
@@ -0,0 +1,15 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
+const htmlmin = require('html-minifier');
+
+module.exports = (content, outputPath) => {
+  if (outputPath.endsWith('.html')) {
+    return htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+    });
+  }
+
+  return content;
+};
