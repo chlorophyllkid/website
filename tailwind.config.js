388819a62ac7b4c2cb43439de9dc1ca8175d63c0
Christopher <chlorophyllkid@gmail.com>
2020-02-25 20:16:36 +0100

    remove old classes and add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
-    extend: {},
+    screens: {
+      xs: '544px',
+      sm: '768px',
+      md: '992px',
+      lg: '1216px',
+      xl: '1440px',
+    },
+    colors: {
+      green: '#008a34',
+    },
+    maxWidth: {
+      text: '80ch',
+    },
+    fill: {
+      current: 'currentColor',
+    },
   },
   variants: {},
   plugins: [],
-}
+};
