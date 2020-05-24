cadb9b80b891ca872afa83e25feef4b508e8df93
Christopher <chlorophyllkid@gmail.com>
2020-05-24 22:40:07 +0200

    add another test function

--- functions/testFunction.js
+++ functions/testFunction.js
@@ -2,7 +2,9 @@ import fetch from 'node-fetch';
 
 const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
-exports.handler = async () => fetch(API_ENDPOINT)
-  .then((response) => response.json())
-  .then((data) => ({ statusCode: 200, body: data }))
-  .catch((error) => ({ statusCode: 422, body: error }));
+exports.handler = async () => {
+  return fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => ({ statusCode: 200, body: data }))
+    .catch((error) => ({ statusCode: 422, body: error }));
+};
