d569a4f5da7cd3aedbb9e1d08f27bf3d47897a85
Chlorophyllkid <chlorophyllkid@gmail.com>
2020-05-26 08:50:28 +0200

    switch APIs back

--- functions/bentTestFunction.js
+++ functions/bentTestFunction.js
@@ -1,14 +1,14 @@
 const bent = require('bent');
 
 const getJSON = bent('json');
-// const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 
-const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines');
-const params = {
-  country: 'de',
-  apiKey: '96eb5c7142df4063b326c651073940ab',
-};
-Object.keys(params).forEach((key) => API_ENDPOINT.searchParams.append(key, params[key]));
+// const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines');
+// const params = {
+//   country: 'de',
+//   apiKey: '96eb5c7142df4063b326c651073940ab',
+// };
+// Object.keys(params).forEach((key) => API_ENDPOINT.searchParams.append(key, params[key]));
 
 exports.handler = async (event, context, callback) => {
   try {
--- functions/fetchTestFunction.js
+++ functions/fetchTestFunction.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch');
 
-// const API_ENDPOINT = 'https://icanhazdadjoke.com/';
-const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines');
-const params = {
-  country: 'de',
-  apiKey: '96eb5c7142df4063b326c651073940ab',
-};
-Object.keys(params).forEach((key) => API_ENDPOINT.searchParams.append(key, params[key]));
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+// const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines');
+// const params = {
+//   country: 'de',
+//   apiKey: '96eb5c7142df4063b326c651073940ab',
+// };
+// Object.keys(params).forEach((key) => API_ENDPOINT.searchParams.append(key, params[key]));
 
-exports.handler = async (event, context, callback) => fetch(API_ENDPOINT)
+exports.handler = async (event, context, callback) => fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
   .then((response) => response.json())
   .then((data) => callback(null, {
     statusCode: 200,
