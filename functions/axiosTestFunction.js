40447cfc0d246cfa651c2128346fffd307336b9a
Christopher <chlorophyllkid@gmail.com>
2020-05-27 22:50:44 +0200

    test different axios approach

--- functions/axiosTestFunction.js
+++ functions/axiosTestFunction.js
@@ -4,10 +4,14 @@ const axios = require('axios');
 const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
 exports.handler = async (event, context, callback) => {
-  try {
-    const response = await axios.get(API_ENDPOINT);
-    callback(null, { statusCode: 200, body: response });
-  } catch (error) {
-    callback(null, { statusCode: 422, body: String(error) });
-  }
+  axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: res.data.title,
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
