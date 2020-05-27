98f6ff83a7504e1aef586cb66da861e4d48528db
Christopher <chlorophyllkid@gmail.com>
2020-05-27 22:02:15 +0200

    add axios test

--- functions/axiosTestFunction.js
+++ functions/axiosTestFunction.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 // const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab');
 
 exports.handler = async (event, context, callback) => {
 
-  return fetch(API_ENDPOINT, { headers: { Accept: 'application/json' } })
+  return axios.get(API_ENDPOINT)
     .then((response) => response.json())
     .then((data) => callback(null, {
       statusCode: 200,
--- functions/bentTestFunction.js
+++ functions/bentTestFunction.js
@@ -1,14 +1,14 @@
 const bent = require('bent');
 
 const getJSON = bent('json');
-// const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 
+// const API_ENDPOINT = 'https://icanhazdadjoke.com/';
 const API_ENDPOINT = new URL('https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab');
 
 exports.handler = async (event, context, callback) => {
   try {
     const response = await getJSON(API_ENDPOINT);
-    callback(null, { statusCode: 200, body: response.joke });
+    callback(null, { statusCode: 200, body: response });
   } catch (error) {
     callback(null, { statusCode: 422, body: error });
   }
