550013df88f59308008d79272b8cbde1f72d905d
Christopher <chlorophyllkid@gmail.com>
2020-05-27 22:39:45 +0200

    rebuild to try catch

--- functions/axiosTestFunction.js
+++ functions/axiosTestFunction.js
@@ -4,14 +4,10 @@ const axios = require('axios');
 const API_ENDPOINT = 'https://newsapi.org/v2/top-headlines?country=de&apiKey=96eb5c7142df4063b326c651073940ab';
 
 exports.handler = async (event, context, callback) => {
-
-  return axios.get(API_ENDPOINT)
-    .then((response) => {
-
-      return callback(null, {
-        statusCode: 200,
-        body: response.data,
-      });
-    })
-    .catch((error) => callback(null, { statusCode: 422, body: String(error) }));
+  try {
+    const response = await axios.get(API_ENDPOINT);
+    callback(null, { statusCode: 200, body: response });
+  } catch (error) {
+    callback(null, { statusCode: 422, body: String(error) });
+  }
 };
