3170aacc6b4c93af502a246fd9aad58db3c20592
Christopher <chlorophyllkid@gmail.com>
2022-10-15 15:23:08 +0200

    remove unnecessary packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-const OpenProps = require('open-props')
-
 module.exports = {
   plugins: [
-    require('postcss-jit-props')(OpenProps),
     require('postcss-import'),
     require('postcss-nesting'),
     require('autoprefixer'),
